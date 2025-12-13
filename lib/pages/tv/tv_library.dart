b57d4f47347d0da6c94e1070a3d9c86e96e8c2aa
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 14:34:35 -0700

    Fix formatting of errorBuilder in _TvLibraryState

--- lib/pages/tv/tv_library.dart
+++ lib/pages/tv/tv_library.dart
@@ -174,7 +174,7 @@ class _TvLibraryState extends ConsumerState<TvLibrary> {
                   item.getPrimaryImageUrl(serverUrl, width: 250),
                   width: double.infinity,
                   fit: BoxFit.cover,
-                  errorBuilder: (_, __, ___) => Container(
+                  errorBuilder: (_, _, ___) => Container(
                     color: AppColors.surface,
                     child: const Center(
                       child: Icon(Icons.movie_outlined, size: 32),
