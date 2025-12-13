1c9d008d5f0ed903f9413326ba90461b9767fb7e
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 14:31:46 -0700

    Fix parameter name in separatorBuilder for libraries row

--- lib/pages/mobile/mobile_home.dart
+++ lib/pages/mobile/mobile_home.dart
@@ -413,7 +413,7 @@ class _MobileHomeState extends ConsumerState<MobileHome> {
         padding: const EdgeInsets.symmetric(horizontal: 16),
         scrollDirection: Axis.horizontal,
         itemCount: libraries.length,
-        separatorBuilder: (_, __) => const SizedBox(width: 12),
+        separatorBuilder: (_, _) => const SizedBox(width: 12),
         itemBuilder: (context, index) {
           final library = libraries[index];
           return _buildLibraryCard(library, serverUrl, index);
