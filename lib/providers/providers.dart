106bcf1d61b9f514e9fce4b6239cf3f66b49405b
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 14:22:00 -0700

    Hide specific media provider classes in providers.dart for cleaner exports

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -2,4 +2,4 @@ export 'auth_provider.dart';
 export 'library_provider.dart';
 export 'player_provider.dart';
 export 'settings_provider.dart';
-export 'media_providers.dart';
+export 'media_providers.dart' hide libraryContentProvider, LibraryContentState, LibraryContentNotifier;
