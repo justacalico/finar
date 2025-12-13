6a9563ed5e33d288b6c295e2d91a07810e0bfb00
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 14:32:48 -0700

    Fix formatting of separatorBuilder in _TvDetailState

--- lib/pages/tv/tv_detail.dart
+++ lib/pages/tv/tv_detail.dart
@@ -404,7 +404,7 @@ class _TvDetailState extends ConsumerState<TvDetail> {
     return episodesAsync.when(
       data: (episodes) => ListView.separated(
         itemCount: episodes.length,
-        separatorBuilder: (_, __) => const SizedBox(height: 12),
+        separatorBuilder: (_, _) => const SizedBox(height: 12),
         itemBuilder: (context, index) {
           final episode = episodes[index];
           final isSelected =
