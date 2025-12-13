e478cdc71ed8fe2f5698d2f584c349076a536931
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 14:01:38 -0700

    Add pages structure for desktop, mobile, and TV layouts

---
+++ lib/pages/pages.dart
@@ -0,0 +1,17 @@
+// Desktop pages
+export 'desktop/desktop_home.dart';
+export 'desktop/desktop_library.dart';
+export 'desktop/desktop_detail.dart';
+export 'desktop/desktop_player.dart';
+
+// Mobile pages
+export 'mobile/mobile_home.dart';
+export 'mobile/mobile_library.dart';
+export 'mobile/mobile_detail.dart';
+export 'mobile/mobile_player.dart';
+
+// TV pages
+export 'tv/tv_home.dart';
+export 'tv/tv_library.dart';
+export 'tv/tv_detail.dart';
+export 'tv/tv_player.dart';
