df4d37ac5611702b5219cee61ab2712ac4da50b5
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 16:54:52 -0700

    Remove unused import for settings_provider in TvSettings

--- lib/pages/tv/tv_settings.dart
+++ lib/pages/tv/tv_settings.dart
@@ -6,7 +6,6 @@ import '../../core/theme/text_styles.dart';
 import '../../core/theme/app_theme.dart';
 import '../../core/utils/platform_detector.dart';
 import '../../providers/providers.dart';
-import '../../providers/settings_provider.dart';
 
 class TvSettings extends ConsumerStatefulWidget {
   const TvSettings({super.key});
