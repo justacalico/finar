3744eedb56b5bbbf53ddfe5bc35806d137c80076
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 14:33:22 -0700

    Fix formatting of errorBuilder in _TvHomeState

--- lib/pages/tv/tv_home.dart
+++ lib/pages/tv/tv_home.dart
@@ -87,7 +87,7 @@ class _TvHomeState extends ConsumerState<TvHome> {
           Image.network(
             item.getBackdropImageUrl(serverUrl, width: 1920),
             fit: BoxFit.cover,
-            errorBuilder: (_, __, ___) =>
+            errorBuilder: (_, _, ___) =>
                 Container(color: AppColors.background),
           ),
           Container(
