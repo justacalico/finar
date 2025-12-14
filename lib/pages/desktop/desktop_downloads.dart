5881f83f008782087f825600a3b94681fc227785
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-14 12:38:52 -0700

    Fix error builder in _DownloadGridCard to streamline image loading fallback

--- lib/pages/desktop/desktop_downloads.dart
+++ lib/pages/desktop/desktop_downloads.dart
@@ -911,7 +911,7 @@ class _DownloadGridCardState extends State<_DownloadGridCard> {
             return Image.file(
               file,
               fit: BoxFit.cover,
-              errorBuilder: (_, _, ___) => _buildNetworkPoster(download, serverUrl),
+              errorBuilder: (_, _, _) => _buildNetworkPoster(download, serverUrl),
             );
           }
           return _buildNetworkPoster(download, serverUrl);
