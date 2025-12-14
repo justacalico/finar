9e1f1e807b211c3abe29f780fb0f6871378e961e
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 18:04:15 -0800

    Fix parameter name in errorBuilder for image loading in MobileMiniPlayer

--- lib/widgets/music_player_bar.dart
+++ lib/widgets/music_player_bar.dart
@@ -74,7 +74,7 @@ class MobileMiniPlayer extends ConsumerWidget {
                         child: Image.network(
                           item.getPrimaryImageUrl(serverUrl, width: 100),
                           fit: BoxFit.cover,
-                          errorBuilder: (_, __, ___) => Container(
+                          errorBuilder: (_, _, ___) => Container(
                             color: AppColors.surface,
                             child: const Icon(
                               Icons.music_note,
