7b8df42cb38eacb0e8483d14b5c604e1b3a5fbac
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 18:06:29 -0800

    Fix parameter name in errorBuilder for image loading in TvMusicLibrary

--- lib/pages/tv/tv_music_library.dart
+++ lib/pages/tv/tv_music_library.dart
@@ -683,7 +683,7 @@ class _TvMusicLibraryState extends ConsumerState<TvMusicLibrary>
                   child: Image.network(
                     artist.getPrimaryImageUrl(serverUrl, width: 300),
                     fit: BoxFit.cover,
-                    errorBuilder: (_, _, ___) => Container(
+                    errorBuilder: (_, _, _) => Container(
                       color: AppColors.surface,
                       child: const Icon(
                         Icons.person,
