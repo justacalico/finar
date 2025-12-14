40bd1cf6818e9ffcd87383fdbc8051c6f37223b5
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 18:02:27 -0800

    Fix errorBuilder parameter name in _buildTrackItem method

--- lib/pages/desktop/desktop_music_library.dart
+++ lib/pages/desktop/desktop_music_library.dart
@@ -453,7 +453,7 @@ class _DesktopMusicLibraryState extends ConsumerState<DesktopMusicLibrary>
                           child: Image.network(
                             track.getPrimaryImageUrl(serverUrl, width: 100),
                             fit: BoxFit.cover,
-                            errorBuilder: (_, _, ___) => Container(
+                            errorBuilder: (_, _, _) => Container(
                               color: AppColors.surface,
                               child: const Icon(
                                 Icons.music_note,
