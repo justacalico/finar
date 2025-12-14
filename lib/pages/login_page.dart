48001ef25f1309a8cf4150e5f152d4b3b89e5258
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 17:48:09 -0800

    Remove unused imports in LoginPage for cleaner code

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../core/theme/app_theme.dart';
