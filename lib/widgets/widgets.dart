80a63b62e98d0c10b02c01c3f695f53ac1b8c002
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 18:25:21 -0800

    Export QueueView widget in widgets.dart for improved accessibility

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -4,5 +4,6 @@ export 'animated_card.dart';
 export 'custom_video_controls.dart';
 export 'glass_chip.dart';
 export 'music_player_bar.dart';
+export 'queue_view.dart';
 // Note: GlassCard is exported from glass_container.dart
 // Note: ShimmerLoading is exported from blur_backdrop.dart
