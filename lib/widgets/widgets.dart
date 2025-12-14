830fd71f8ea2828dcf259da97c66ae2d56a70bd2
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 18:03:37 -0800

    Add export for music_player_bar in widgets.dart

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -3,5 +3,6 @@ export 'blur_backdrop.dart';
 export 'animated_card.dart';
 export 'custom_video_controls.dart';
 export 'glass_chip.dart';
+export 'music_player_bar.dart';
 // Note: GlassCard is exported from glass_container.dart
 // Note: ShimmerLoading is exported from blur_backdrop.dart
