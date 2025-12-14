d5eb3198ca6292a37b2b7114d092eae481182edc
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 19:53:23 -0700

    Remove unused import of 'package:flutter/services.dart' in animated_card.dart

--- lib/widgets/animated_card.dart
+++ lib/widgets/animated_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import '../core/theme/colors.dart';
