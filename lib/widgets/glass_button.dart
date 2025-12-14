b7d401637a58b417dfbfbd1dbabd90133c2ea52f
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 19:57:33 -0700

    Refactor GlassButton and GlassIconButton to support focus and keyboard events

--- lib/widgets/glass_button.dart
+++ lib/widgets/glass_button.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../core/theme/colors.dart';
 import '../core/theme/app_theme.dart';
+import '../core/services/controller_service.dart';
 import 'glass_container.dart';
 
-/// A glass-styled button with blur effect
-class GlassButton extends StatelessWidget {
+/// A glass-styled button with blur effect and focus support
+class GlassButton extends StatefulWidget {
   final Widget child;
   final VoidCallback? onPressed;
   final EdgeInsets padding;
   final double borderRadius;
+  final bool autofocus;
+  final FocusNode? focusNode;
 
   const GlassButton({
     super.key,
@@ -16,30 +20,111 @@ class GlassButton extends StatelessWidget {
     this.onPressed,
     this.padding = const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
     this.borderRadius = AppTheme.radiusMd,
+    this.autofocus = false,
+    this.focusNode,
   });
 
   @override
+  State<GlassButton> createState() => _GlassButtonState();
+}
+
+class _GlassButtonState extends State<GlassButton> {
+  bool _isFocused = false;
+  late FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = widget.focusNode ?? FocusNode();
+    _focusNode.addListener(_onFocusChange);
+  }
+
+  @override
+  void dispose() {
+    _focusNode.removeListener(_onFocusChange);
+    if (widget.focusNode == null) {
+      _focusNode.dispose();
+    }
+    super.dispose();
+  }
+
+  void _onFocusChange() {
+    if (mounted) {
+      setState(() {
+        _isFocused = _focusNode.hasFocus;
+      });
+      if (_isFocused) {
+        Scrollable.ensureVisible(
+          context,
+          alignment: 0.5,
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeOutCubic,
+        );
+      }
+    }
+  }
+
+  KeyEventResult _handleKeyEvent(FocusNode node, KeyEvent event) {
+    if (!ControllerService.isKeyDown(event)) {
+      return KeyEventResult.ignored;
+    }
+
+    final action = ControllerService.getAction(event);
+    if (action == ControllerAction.select) {
+      widget.onPressed?.call();
+      return KeyEventResult.handled;
+    }
+
+    return KeyEventResult.ignored;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: GlassContainer(
-        blur: AppTheme.blurLight,
-        opacity: 0.1,
-        borderRadius: borderRadius,
-        padding: padding,
-        child: child,
+    return Focus(
+      focusNode: _focusNode,
+      autofocus: widget.autofocus,
+      onKeyEvent: _handleKeyEvent,
+      child: GestureDetector(
+        onTap: widget.onPressed,
+        child: AnimatedContainer(
+          duration: AppTheme.durationFast,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(widget.borderRadius),
+            border: _isFocused
+                ? Border.all(color: AppColors.primary, width: 2)
+                : null,
+            boxShadow: _isFocused
+                ? [
+                    BoxShadow(
+                      color: AppColors.primary.withValues(alpha: 0.3),
+                      blurRadius: 8,
+                      spreadRadius: 1,
+                    ),
+                  ]
+                : null,
+          ),
+          child: GlassContainer(
+            blur: AppTheme.blurLight,
+            opacity: _isFocused ? 0.15 : 0.1,
+            borderRadius: widget.borderRadius,
+            padding: widget.padding,
+            child: widget.child,
+          ),
+        ),
       ),
     );
   }
 }
 
-/// A glass-styled icon button
-class GlassIconButton extends StatelessWidget {
+/// A glass-styled icon button with focus support
+class GlassIconButton extends StatefulWidget {
   final IconData icon;
   final VoidCallback? onPressed;
   final double size;
   final Color? iconColor;
   final Color? backgroundColor;
+  final bool autofocus;
+  final FocusNode? focusNode;
 
   const GlassIconButton({
     super.key,
@@ -48,28 +133,109 @@ class GlassIconButton extends StatelessWidget {
     this.size = 44,
     this.iconColor,
     this.backgroundColor,
+    this.autofocus = false,
+    this.focusNode,
   });
 
   @override
+  State<GlassIconButton> createState() => _GlassIconButtonState();
+}
+
+class _GlassIconButtonState extends State<GlassIconButton> {
+  bool _isFocused = false;
+  late FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = widget.focusNode ?? FocusNode();
+    _focusNode.addListener(_onFocusChange);
+  }
+
+  @override
+  void dispose() {
+    _focusNode.removeListener(_onFocusChange);
+    if (widget.focusNode == null) {
+      _focusNode.dispose();
+    }
+    super.dispose();
+  }
+
+  void _onFocusChange() {
+    if (mounted) {
+      setState(() {
+        _isFocused = _focusNode.hasFocus;
+      });
+      if (_isFocused) {
+        Scrollable.ensureVisible(
+          context,
+          alignment: 0.5,
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeOutCubic,
+        );
+      }
+    }
+  }
+
+  KeyEventResult _handleKeyEvent(FocusNode node, KeyEvent event) {
+    if (!ControllerService.isKeyDown(event)) {
+      return KeyEventResult.ignored;
+    }
+
+    final action = ControllerService.getAction(event);
+    if (action == ControllerAction.select) {
+      widget.onPressed?.call();
+      return KeyEventResult.handled;
+    }
+
+    return KeyEventResult.ignored;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: GlassContainer(
-        blur: AppTheme.blurLight,
-        opacity: 0.1,
-        borderRadius: size / 2,
-        padding: EdgeInsets.zero,
-        child: Container(
-          width: size,
-          height: size,
+    return Focus(
+      focusNode: _focusNode,
+      autofocus: widget.autofocus,
+      onKeyEvent: _handleKeyEvent,
+      child: GestureDetector(
+        onTap: widget.onPressed,
+        child: AnimatedContainer(
+          duration: AppTheme.durationFast,
           decoration: BoxDecoration(
-            color: backgroundColor,
             shape: BoxShape.circle,
+            border: _isFocused
+                ? Border.all(color: AppColors.primary, width: 2)
+                : null,
+            boxShadow: _isFocused
+                ? [
+                    BoxShadow(
+                      color: AppColors.primary.withValues(alpha: 0.3),
+                      blurRadius: 8,
+                      spreadRadius: 1,
+                    ),
+                  ]
+                : null,
           ),
-          child: Icon(
-            icon,
-            size: size * 0.5,
-            color: iconColor ?? AppColors.textPrimary,
+          child: GlassContainer(
+            blur: AppTheme.blurLight,
+            opacity: _isFocused ? 0.15 : 0.1,
+            borderRadius: widget.size / 2,
+            padding: EdgeInsets.zero,
+            child: Container(
+              width: widget.size,
+              height: widget.size,
+              decoration: BoxDecoration(
+                color: widget.backgroundColor,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                widget.icon,
+                size: widget.size * 0.5,
+                color: _isFocused
+                    ? AppColors.primary
+                    : (widget.iconColor ?? AppColors.textPrimary),
+              ),
+            ),
           ),
         ),
       ),
