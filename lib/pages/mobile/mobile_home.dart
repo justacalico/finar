9069706c5bbeb30837ef8e912464fa372b56385c
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 19:46:40 -0700

    Refactor bottom navigation animation to use diagonal3Values for improved scaling

--- lib/pages/mobile/mobile_home.dart
+++ lib/pages/mobile/mobile_home.dart
@@ -249,7 +249,7 @@ class _MobileHomeState extends ConsumerState<MobileHome> with SingleTickerProvid
               AnimatedContainer(
                 duration: const Duration(milliseconds: 300),
                 curve: Curves.easeOutCubic,
-                transform: Matrix4.identity()..scale(isSelected ? 1.1 : 1.0, isSelected ? 1.1 : 1.0),
+                transform: Matrix4.diagonal3Values(isSelected ? 1.1 : 1.0, isSelected ? 1.1 : 1.0, 1.0),
                 transformAlignment: Alignment.center,
                 child: Icon(
                   isSelected ? selectedIcon : icon,
