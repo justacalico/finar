aecfd30d1fd4efaaa1deb059154d7d8dfa60a7c9
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 18:32:03 -0800

    Add missing import for MediaItem model in music_player_bar.dart

--- lib/widgets/music_player_bar.dart
+++ lib/widgets/music_player_bar.dart
@@ -4,6 +4,7 @@ import 'package:flutter_animate/flutter_animate.dart';
 import '../core/theme/colors.dart';
 import '../core/theme/text_styles.dart';
 import '../core/theme/app_theme.dart';
+import '../core/api/models/media_item.dart';
 import '../providers/providers.dart';
 import '../providers/library_provider.dart';
 import 'glass_container.dart';
