e27e6fb5285ba94b9e0db3bab6db831cd5642bca
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 18:03:17 -0800

    Fix errorBuilder parameter name in _buildTrackItem method

--- lib/pages/mobile/mobile_music_library.dart
+++ lib/pages/mobile/mobile_music_library.dart
@@ -291,7 +291,7 @@ class _MobileMusicLibraryState extends ConsumerState<MobileMusicLibrary>
                       child: Image.network(
                         track.getPrimaryImageUrl(serverUrl, width: 100),
                         fit: BoxFit.cover,
-                        errorBuilder: (_, _, ___) => Container(
+                        errorBuilder: (_, _, _) => Container(
                           color: AppColors.surface,
                           child: Center(
                             child: Text(
