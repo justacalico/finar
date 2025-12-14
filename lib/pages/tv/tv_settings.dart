12fd84fe78d445b1829ff68ed02444e02ec8e36d
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 18:17:58 -0700

    Add package imports for package_info_plus and url_launcher in TvSettings

--- lib/pages/tv/tv_settings.dart
+++ lib/pages/tv/tv_settings.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../../core/theme/colors.dart';
 import '../../core/theme/text_styles.dart';
 import '../../core/theme/app_theme.dart';
