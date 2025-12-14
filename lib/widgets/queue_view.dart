601c594300e11df0f3b142a87171aff1957d0e1e
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-13 18:27:49 -0800

    Remove unused imports in QueueView for cleaner code

--- lib/widgets/queue_view.dart
+++ lib/widgets/queue_view.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../providers/player_provider.dart';
 import '../providers/providers.dart';
-import '../core/api/jellyfin_api.dart';
 import '../core/api/models/media_item.dart';
-import 'glass_container.dart';
 
 class QueueView extends ConsumerWidget {
   const QueueView({super.key});
