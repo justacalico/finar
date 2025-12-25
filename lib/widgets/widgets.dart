fbf276d6438b950ff50b2f4bf811622e8d13b885
HttpAnimations <shawndouglasyt@gmail.com>
2025-12-24 21:07:45 -0700

    Add export for TvFocusable widget: Include TV-optimized focusable component in widgets export

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -5,5 +5,6 @@ export 'custom_video_controls.dart';
 export 'glass_chip.dart';
 export 'music_player_bar.dart';
 export 'queue_view.dart';
+export 'tv_focusable.dart';
 // Note: GlassCard is exported from glass_container.dart
 // Note: ShimmerLoading is exported from blur_backdrop.dart
