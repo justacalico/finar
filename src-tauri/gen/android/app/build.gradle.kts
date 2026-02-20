e3617d8acd68ee6a720c78a8dc3c3c8b47f8bd63
HttpAnimations <shawndouglasyt@gmail.com>
2026-02-19 23:10:46 -0700

    android

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -15,10 +15,10 @@ val tauriProperties = Properties().apply {
 
 android {
     compileSdk = 36
-    namespace = "com.httpanimations.finar_app"
+    namespace = "com.openlyst.finar"
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
-        applicationId = "com.httpanimations.finar_app"
+        applicationId = "com.openlyst.finar"
         minSdk = 24
         targetSdk = 36
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
